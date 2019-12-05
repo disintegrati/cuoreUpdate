1cb29ab3c57e982e17dca85c4437f5af425c9b92
plcdnl <polaccodaniele@gmail.com>
2019-12-05 22:58:18 +0100

    first commit

--- index.js
+++ index.js
@@ -2,16 +2,38 @@ var fs = require('fs'),
     request = require('request'),
     ig = require('instagram-scraping');
 
-ig.scrapeTag('cuoredinapoli').then(result => {
-  for (let i = 0; i < 27; i++) {
-    download(result.medias[i].display_url, './images/'+i+'.jpg', function(){
-      console.log('image number '+i+ ' downloaded');
-    });
-  }
-});
+setInterval(function(){
+  ig.scrapeTag('cuoredinapoli').then(result => {
+    for (let i = 0; i < 27; i++) {
+      download(result.medias[i].display_url, './images/'+i+'.jpg', function(){
+        console.log('image number '+i+ ' downloaded');
+      });
+    }
+  });
+},30000);
 
 function download(uri, filename, callback){
   request.head(uri, function(err, res, body){
     request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
   });
 };
+
+
+var express = require('express');
+var app = express();
+var path = require("path");
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname)));
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port, function() {
+  console.log('Our app is running on port:' + port);
+});
