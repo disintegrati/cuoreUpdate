874d13bf81e9007cd3860f7ece071e986315b0d5
plcdnl <polaccodaniele@gmail.com>
2019-12-05 23:35:38 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-var fs = require('fs'),
-    request = require('request'),
-    ig = require('instagram-scraping');
-
-setInterval(function(){
-  ig.scrapeTag('cuoredinapoli').then(result => {
-    for (let i = 0; i < 27; i++) {
-      download(result.medias[i].display_url, './images/'+i+'.jpg', function(){
-        console.log('image number '+i+ ' downloaded');
-      });
-    }
-  });
-},30000);
-
-function download(uri, filename, callback){
-  request.head(uri, function(err, res, body){
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
-};
-
-
+var fs = require('fs');
+var request = require('request');
+var ig = require('instagram-scraping');
 var express = require('express');
 var app = express();
 var path = require("path");
@@ -29,11 +11,25 @@ var port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname)));
 
-
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(port, function() {
   console.log('Our app is running on port:' + port);
+  setInterval(function(){
+    ig.scrapeTag('cuoredinapoli').then(result => {
+      for (let i = 0; i < 27; i++) {
+        download(result.medias[i].display_url, './images/'+i+'.jpg', function(){
+          console.log('image number '+i+ ' downloaded');
+        });
+      }
+    });
+  },30000);
 });
+
+function download(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
